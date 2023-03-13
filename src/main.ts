c172890a47d5508c4625f9a3614fd8afea59d907
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-03-13 09:48:59 +0100

    fix: remove unused icon imports

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import {faBars, faUserSecret, faEnvelope, faServer, faCode, faCar, faHouseLaptop} from '@fortawesome/free-solid-svg-icons'
-import {faFacebook, faInstagram, faLinkedinIn, faGithub, faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope, faCode, faCar, faHouseLaptop} from '@fortawesome/free-solid-svg-icons'
+import {faInstagram, faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faUserSecret, faBars, faEnvelope, faLinkedinIn, faFacebook, faGithub, faWhatsapp, faInstagram, faServer, faCode, faCar, faHouseLaptop)
+library.add(faEnvelope, faLinkedinIn, faGithub, faInstagram, faCode, faCar, faHouseLaptop)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
