e83ddd8406c5c821beb6ecb9d3e6b30a8372e432
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-03-13 07:37:53 +0100

    fix: remove imprint from footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,10 +1,5 @@
 <template>
   <footer class="bottom-0 flex flex-col gap-3 justify-between items-center bg-[#161616] px-6 py-6 text-[#686868]">
-    <div class="flex items-center gap-3">
-      <a>Impressum</a>
-      <hr class="w-6 h-0.5 bg-[#686868] border-0 rounded">
-      <a>Datenschutz</a>
-    </div>
     <a>&copy; goekhantopcu.de</a>
   </footer>
 </template>
