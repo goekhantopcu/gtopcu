1f4f17de32d459487331a861cc999787f9748d7c
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-03-13 07:53:37 +0100

    fix: remove rotation of company icon on hover

--- src/components/Companies.vue
+++ src/components/Companies.vue
@@ -3,7 +3,7 @@
     <div class="col-span-2 flex flex-col justify-center items-center" v-for="company in companies" :key="company.id">
       <a :href="company.url" target="_blank" :title="company.alt">
         <img :src="company.cover"
-             class="object-cover h-[1.25rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3.5rem] hover:cursor-pointer grayscale hover:grayscale-0 ease-in-out duration-700 hover:origin-center hover:rotate-12 hover:scale-110"
+             class="object-cover h-[1.25rem] sm:h-[2.5rem] md:h-[3rem] lg:h-[3.5rem] hover:cursor-pointer grayscale hover:grayscale-0 ease-in-out duration-700 hover:origin-center hover:scale-110"
              :title="company.alt"
              :alt="company.alt">
       </a>
