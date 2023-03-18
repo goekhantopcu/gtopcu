bf3608e86395764c8adfcf11ddeace14ae73e650
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2023-03-18 23:44:01 +0100

    fix: transform SkillModel to interface

--- src/models/skill-model.ts
+++ src/models/skill-model.ts
@@ -1,11 +1,5 @@
-export class SkillModel {
-    readonly icon: string;
-    readonly title: string
-    readonly description: string;
-
-    constructor(icon: string, title: string, description: string) {
-        this.icon = icon;
-        this.title = title;
-        this.description = description;
-    }
+export interface SkillModel {
+    icon: string;
+    title: string
+    description: string;
 }
